dba6e738610e623aff375de466ced636e0c212aa
Leanne Hao <Leanne.Hao@shell.com>
2020-12-27 04:47:28 -0600

    yml

--- run_docker.sh
+++ run_docker.sh
@@ -4,12 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker image build -t leannehao/udacityaiengeer:v0.1
+docker image build . --tag leannehao/udacityaiengeer:v0.2
 
 # Step 2: 
 # List docker images
-docker image ls leannehao/udacityaiengeer:v0.1
+docker image ls leannehao/udacityaiengeer:v0.2
 
 # Step 3: 
 # Run flask app
-docker run -it leannehao/udacityaiengeer:v0.1
+docker run -p 8000:80 -it leannehao/udacityaiengeer:v0.2
